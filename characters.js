fee64080605ca0e8ff06f09e25e1639a03fe2b61
SpaceDmentia <61288858+SpaceDmentia@users.noreply.github.com>
2020-08-04 15:19:35 -0400

    ébauche

---
+++ characters.js
@@ -0,0 +1,63 @@
+var = characters[{
+        name: "D.va";
+        realm: "Overwatch";
+        gender: "Female";
+        race: "Human";
+        class: "Bruiser";
+        hair: "Brown";
+    },
+
+    {
+        name: "Butcher";
+        realm: "Diablo";
+        gender: "Male";
+        race: "Demon";
+        class: "Tank";
+        hair: "No";
+    },
+
+    {
+        name: "Illidan";
+        realm: "World of Warcraft";
+        gender: "Male";
+        race: "Demon";
+        class: "Assassin";
+        hair: "Black";
+    }, {
+        name: "Zagara";
+        realm: "Starcraft";
+        gender: "Female";
+        race: "Zerg";
+        class: "Assassin";
+        hair: "No";
+    }, {
+        name: "Garrosh";
+        realm: "World of Warcraft";
+        gender: "Male";
+        race: "Orc";
+        class: "Tank";
+        hair: "No";
+    }, {
+        name: "Abathur";
+        realm: "Starcraft";
+        gender: "Male";
+        race: "Zerg";
+        class: "Support";
+        hair: "No";
+    }, {
+        name: "Li-Ming";
+        realm: "Diablo";
+        gender: "Female";
+        race: "Nephalem";
+        class: "Assassin";
+        hair: "Black";
+    }, {
+        name: "Deathwing";
+        realm: "World of Warcraft";
+        gender: "Male";
+        race: "Dragon";
+        class: "Bruiser";
+        hair: "No";
+    },
+
+]
